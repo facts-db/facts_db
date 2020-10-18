ac8e1be5250d0bba6a0a49030f1a00fcc78a2a67
Thomas de Grivel <thoxdg@gmail.com>
2020-10-18 20:03:08 +0200

    tests: check facts remove

--- tests/check_facts.c
+++ tests/check_facts.c
@@ -123,22 +123,115 @@ START_TEST (test_facts_add_fact_ten)
 }
 END_TEST
 
+void setup_remove_fact ()
+{
+        g_f = new_facts();
+        facts_add_fact(g_f, new_fact("a", "b", "c"));
+        facts_add_fact(g_f, new_fact("b", "c", "d"));
+        facts_add_fact(g_f, new_fact("c", "d", "e"));
+        facts_add_fact(g_f, new_fact("d", "e", "f"));
+        facts_add_fact(g_f, new_fact("e", "f", "g"));
+        facts_add_fact(g_f, new_fact("f", "g", "h"));
+        facts_add_fact(g_f, new_fact("g", "h", "i"));
+        facts_add_fact(g_f, new_fact("h", "i", "j"));
+        facts_add_fact(g_f, new_fact("i", "j", "k"));
+        facts_add_fact(g_f, new_fact("j", "k", "l"));
+}
+
+void teardown_remove_fact ()
+{
+        delete_facts(g_f);
+        g_f = NULL;
+}
+
+START_TEST (test_facts_remove_fact_one)
+{
+        s_fact *a = new_fact("a", "b", "c");
+        ck_assert(facts_count(g_f) == 10);
+        ck_assert(facts_remove_fact(g_f, a));
+        ck_assert(facts_count(g_f) == 9);
+        ck_assert(facts_remove_fact(g_f, a) == NULL);
+        ck_assert(facts_count(g_f) == 9);
+}
+END_TEST
+
+START_TEST (test_facts_remove_fact_two)
+{
+        s_fact *a  = new_fact("a", "b", "c");
+        s_fact *b  = new_fact("b", "c", "d");
+        ck_assert(facts_count(g_f) == 10);
+        ck_assert(facts_remove_fact(g_f, a));
+        ck_assert(facts_count(g_f) == 9);
+        ck_assert(facts_remove_fact(g_f, b));
+        ck_assert(facts_count(g_f) == 8);
+        ck_assert(facts_remove_fact(g_f, a) == NULL);
+        ck_assert(facts_count(g_f) == 8);
+        ck_assert(facts_remove_fact(g_f, b) == NULL);
+        ck_assert(facts_count(g_f) == 8);
+}
+END_TEST
+
+START_TEST (test_facts_remove_fact_ten)
+{
+        s_fact *a = new_fact("a", "b", "c");
+        s_fact *b = new_fact("b", "c", "d");
+        s_fact *c = new_fact("c", "d", "e");
+        s_fact *d = new_fact("d", "e", "f");
+        s_fact *e = new_fact("e", "f", "g");
+        s_fact *f = new_fact("f", "g", "h");
+        s_fact *g = new_fact("g", "h", "i");
+        s_fact *h = new_fact("h", "i", "j");
+        s_fact *i = new_fact("i", "j", "k");
+        s_fact *j = new_fact("j", "k", "l");
+        ck_assert(facts_count(g_f) == 10);
+        ck_assert(facts_remove_fact(g_f, a));
+        ck_assert(facts_count(g_f) == 9);
+        ck_assert(facts_remove_fact(g_f, b));
+        ck_assert(facts_count(g_f) == 8);
+        ck_assert(facts_remove_fact(g_f, c));
+        ck_assert(facts_count(g_f) == 7);
+        ck_assert(facts_remove_fact(g_f, d));
+        ck_assert(facts_count(g_f) == 6);
+        ck_assert(facts_remove_fact(g_f, e));
+        ck_assert(facts_count(g_f) == 5);
+        ck_assert(facts_remove_fact(g_f, f));
+        ck_assert(facts_count(g_f) == 4);
+        ck_assert(facts_remove_fact(g_f, g));
+        ck_assert(facts_count(g_f) == 3);
+        ck_assert(facts_remove_fact(g_f, h));
+        ck_assert(facts_count(g_f) == 2);
+        ck_assert(facts_remove_fact(g_f, i));
+        ck_assert(facts_count(g_f) == 1);
+        ck_assert(facts_remove_fact(g_f, j));
+        ck_assert(facts_count(g_f) == 0);
+}
+END_TEST
+
 Suite * fact_suite(void)
 {
     Suite *s;
     TCase *tc_init;
     TCase *tc_add_fact;
+    TCase *tc_remove_fact;
     s = suite_create("Fact");
     tc_init = tcase_create("Init");
     tcase_add_test(tc_init, test_facts_init_destroy);
     tcase_add_test(tc_init, test_facts_new_delete);
     suite_add_tcase(s, tc_init);
     tc_add_fact = tcase_create("Add fact");
-    tcase_add_checked_fixture(tc_add_fact, setup_add_fact, teardown_add_fact);
+    tcase_add_checked_fixture(tc_add_fact, setup_add_fact,
+                              teardown_add_fact);
     tcase_add_test(tc_add_fact, test_facts_add_fact_one);
     tcase_add_test(tc_add_fact, test_facts_add_fact_two);
     tcase_add_test(tc_add_fact, test_facts_add_fact_ten);
     suite_add_tcase(s, tc_add_fact);
+    tc_remove_fact = tcase_create("Remove fact");
+    tcase_add_checked_fixture(tc_remove_fact, setup_remove_fact,
+                              teardown_remove_fact);
+    tcase_add_test(tc_remove_fact, test_facts_remove_fact_one);
+    tcase_add_test(tc_remove_fact, test_facts_remove_fact_two);
+    tcase_add_test(tc_remove_fact, test_facts_remove_fact_ten);
+    suite_add_tcase(s, tc_remove_fact);
     return s;
 }
 
