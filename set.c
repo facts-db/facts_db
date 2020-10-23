bc31002c4b04dae44411ab84a5fd1c865a3ef5e2
Thomas de Grivel <thoxdg@gmail.com>
2020-10-23 18:00:40 +0200

    set based on Google farmhash

---
+++ set.c
@@ -0,0 +1,235 @@
+/*
+ *  facts_db - in-memory graph database
+ *  Copyright 2020 Thomas de Grivel <thoxdg@gmail.com>
+ *
+ *  Permission to use, copy, modify, and distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include "farmhash.h"
+#include "set.h"
+
+s_set_item * new_set_item (size_t len, void *data, size_t hash,
+                           s_set_item *next)
+{
+        s_set_item *i = malloc(sizeof(s_set_item));
+        if (i) {
+                i->len = len;
+                i->data = data;
+                i->hash = hash;
+                i->next = next;
+        }
+        return i;
+}
+
+void set_init (s_set *set, size_t max)
+{
+        assert(set);
+        assert(max > 0);
+        set->max = max;
+        set->items = calloc(max, sizeof(s_set_item));
+        set->count = 0;
+}
+
+void set_destroy (s_set *set)
+{
+        size_t i;
+        assert(set);
+        for (i = 0; i < set->max; i++) {
+                s_set_item *j = set->items[i].next;
+                while (j) {
+                        s_set_item *k = j;
+                        j = j->next;
+                        free(k);
+                }
+        }
+        set->max = 0;
+        free(set->items);
+        set->items = NULL;
+        set->count = 0;
+        set->collisions = 0;
+}
+
+s_set * new_set (size_t max)
+{
+        s_set *set;
+        assert(max > 0);
+        set = malloc(sizeof(s_set));
+        if (set)
+                set_init(set, max);
+        return set;
+}
+
+void delete_set (s_set *set)
+{
+        if (set) {
+                set_destroy(set);
+                free(set);
+        }
+}
+
+static s_set_item * set_add_collision (s_set *set, void *data,
+                                       size_t len, size_t hash,
+                                       s_set_item *i)
+{
+        s_set_item *n = new_set_item(len, data, hash, i->next);
+        assert(n);
+        i->next = n;
+        set->count++;
+        set->collisions++;
+        return n;
+}
+
+s_set_item * set_add (s_set *set, void *data, size_t len)
+{
+        size_t hash;
+        assert(set);
+        assert(data);
+        assert(len > 0);
+        hash = farmhash(data, len);
+        return set_add_h(set, data, len, hash);
+}
+
+s_set_item * set_add_h (s_set *set, void *data, size_t len, size_t hash)
+{
+        s_set_item *i;
+        assert(set);
+        assert(data);
+        assert(len > 0);
+        i = set_get_h(set, data, len, hash);
+        if (i)
+                return i;
+        i = set->items + hash % set->max;
+        if (i->len)
+                return set_add_collision(set, data, len, hash, i);
+        i->len = len;
+        i->data = data;
+        i->hash = hash;
+        set->count++;
+        return i;
+}
+
+int set_remove_first (s_set_item *i)
+{
+        s_set_item *j = i->next;
+        if (j) {
+                i->len = j->len;
+                i->data = j->data;
+                i->hash = j->hash;
+                i->next = j->next;
+                free(j);
+        }
+        else {
+                i->len = 0;
+                i->data = 0;
+                i->hash = 0;
+                i->next = 0;
+        }
+        return 1;
+}
+
+int set_remove_linked (s_set_item *item, s_set_item *i)
+{
+        s_set_item **j = &i->next;
+        while (*j && *j != item)
+                j = &(*j)->next;
+        if (!*j)
+                return 0;
+        i = *j;
+        *j = (*j)->next;
+        free(i);
+        return 1;
+}
+
+int set_remove (s_set *set, s_set_item *item)
+{
+        s_set_item *i;
+        assert(set);
+        assert(item);
+        i = set->items + (item->hash % set->max);
+        if (i == item)
+                return set_remove_first(i);
+        else
+                return set_remove_linked(item, i);
+}
+
+s_set_item * set_get (s_set *set, void *data, size_t len)
+{
+        size_t hash;
+        assert(set);
+        assert(data);
+        assert(len > 0);
+        hash = farmhash(data, len);
+        return set_get_h(set, data, len, hash);
+}
+
+s_set_item * set_get_h (s_set *set, void *data, size_t len, size_t hash)
+{
+        s_set_item *i;
+        assert(set);
+        assert(data);
+        assert(len > 0);
+        i = set_get_hash(set, hash);
+        while (i) {
+                if (len == i->len && memcmp(data, i->data, len) == 0)
+                        return i;
+                i = set_get_hash_next(i);
+        }
+        return NULL;
+}
+
+s_set_item * set_get_hash (s_set *set, size_t hash)
+{
+        s_set_item *i;
+        assert(set);
+        i = set->items + (hash % set->max);
+        while (i && i->hash != hash)
+                i = i->next;
+        return i;
+}
+
+s_set_item * set_get_hash_next (s_set_item *item)
+{
+        s_set_item *i;
+        assert(item);
+        i = item->next;
+        while (i && i->hash != item->hash)
+                i = i->next;
+        return i;
+}
+
+void set_resize (s_set *set, size_t max)
+{
+        size_t i;
+        s_set n;
+        if (max == set->max)
+                return;
+        set_init(&n, max);
+        for (i = 0; i < set->max; i++) {
+                s_set_item *item = set->items + i;
+                if (!item->len)
+                        item = NULL;
+                while (item) {
+                        set_add_h(&n, item->data, item->len,
+                                  item->hash);
+                        item = item->next;
+                }
+        }
+        set_destroy(set);
+        set->max = n.max;
+        set->items = n.items;
+        set->count = n.count;
+        set->collisions = n.collisions;
+}
---
+++ set.h
@@ -0,0 +1,73 @@
+/*
+ *  facts_db - in-memory graph database
+ *  Copyright 2020 Thomas de Grivel <thoxdg@gmail.com>
+ *
+ *  Permission to use, copy, modify, and distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifndef SET_H
+#define SET_H
+
+typedef struct set_item s_set_item;
+
+struct set_item {
+        size_t len;
+        void *data;
+        size_t hash;
+        s_set_item *next;
+};
+
+s_set_item * new_set_item (size_t len, void *data, size_t hash, s_set_item *next);
+
+typedef struct set {
+        size_t max;
+        s_set_item *items;
+        size_t count;
+        size_t collisions;
+} s_set;
+
+void        set_init (s_set *set,
+                      size_t max);
+
+void        set_destroy (s_set *set);
+
+s_set * new_set (size_t max);
+
+void delete_set (s_set *set);
+
+s_set_item * set_add (s_set *set,
+                      void *data,
+                      size_t len);
+
+s_set_item * set_add_h (s_set *set,
+                        void *data,
+                        size_t len,
+                        size_t hash);
+
+int          set_remove (s_set *set,
+                         s_set_item *item);
+
+s_set_item * set_get (s_set *set,
+                      void *data,
+                      size_t len);
+
+s_set_item * set_get_h (s_set *set,
+                        void *data,
+                        size_t len,
+                        size_t hash);
+
+s_set_item * set_get_hash (s_set *set,
+                           size_t hash);
+
+s_set_item * set_get_hash_next (s_set_item *item);
+
+#endif
