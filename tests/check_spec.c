f9f24b9c05211503be7af4cd09eb2f850593d875
Thomas de Grivel <thoxdg@gmail.com>
2020-10-27 10:49:09 +0100

    check_spec

--- tests/check_spec.c
+++ tests/check_spec.c
@@ -22,8 +22,51 @@ START_TEST (test_spec_cursor_empty)
 {
         s_spec_cursor c;
         s_fact f;
+        spec_cursor_init(&c, (const char *[]) {NULL, NULL});
+        ck_assert(!spec_cursor_next(&c, &f));
+}
+END_TEST
+
+START_TEST (test_spec_cursor_one)
+{
+        s_spec_cursor c;
+        s_fact f;
         spec_cursor_init(&c, (const char *[]) {
-                        NULL, NULL});
+                        "a", "b", "c", NULL, NULL});
+        ck_assert(spec_cursor_next(&c, &f));
+        ck_assert(!strcmp(f.s, "a"));
+        ck_assert(!strcmp(f.p, "b"));
+        ck_assert(!strcmp(f.o, "c"));
+        ck_assert(!spec_cursor_next(&c, &f));
+}
+END_TEST
+
+START_TEST (test_spec_cursor_two)
+{
+        s_spec_cursor c;
+        s_fact f;
+        spec_cursor_init(&c, (const char *[]) {
+                        "a", "b", "c", "d", "e", NULL, NULL});
+        ck_assert(spec_cursor_next(&c, &f));
+        ck_assert(!strcmp(f.s, "a"));
+        ck_assert(!strcmp(f.p, "b"));
+        ck_assert(!strcmp(f.o, "c"));
+        ck_assert(spec_cursor_next(&c, &f));
+        ck_assert(!strcmp(f.s, "a"));
+        ck_assert(!strcmp(f.p, "d"));
+        ck_assert(!strcmp(f.o, "e"));
+        ck_assert(!spec_cursor_next(&c, &f));
+        spec_cursor_init(&c, (const char *[]) {
+                        "a", "b", "c", NULL,
+                        "b", "d", "e", NULL, NULL});
+        ck_assert(spec_cursor_next(&c, &f));
+        ck_assert(!strcmp(f.s, "a"));
+        ck_assert(!strcmp(f.p, "b"));
+        ck_assert(!strcmp(f.o, "c"));
+        ck_assert(spec_cursor_next(&c, &f));
+        ck_assert(!strcmp(f.s, "b"));
+        ck_assert(!strcmp(f.p, "d"));
+        ck_assert(!strcmp(f.o, "e"));
         ck_assert(!spec_cursor_next(&c, &f));
 }
 END_TEST
@@ -35,6 +78,8 @@ Suite * spec_suite(void)
         s = suite_create("Spec");
         tc_cursor = tcase_create("Cursor");
         tcase_add_test(tc_cursor, test_spec_cursor_empty);
+        tcase_add_test(tc_cursor, test_spec_cursor_one);
+        tcase_add_test(tc_cursor, test_spec_cursor_two);
         suite_add_tcase(s, tc_cursor);
         return s;
 }
